d000839a1a7b0f71e4a74c7d0aceb36760792373
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-16 18:57:18 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`RT公式document ${siteConfig.title}`}
+      description="何かわからないことがありましたら、こちらをご覧ください。">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
