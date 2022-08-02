608dce177d7849eb600876127708284debe86290
tasuren <tasuren_0618@yahoo.co.jp>
2022-08-02 10:21:07 +0900

    update: たくさん書き込んだ。

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
             className="button button--secondary button--lg"
             to="/docs/intro">
             <Translate description="RT turoial">
-              RTチュートリアル
+              ドキュメントを見る
             </Translate>
           </Link>
         </div>
