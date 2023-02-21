0485dee480e1a81f1b8ab0c78426f1f24dcdcf8a
Takagi Tasuku <tasuren@outlook.jp>
2023-02-21 13:27:22 +0900

    fix: 「このページを編集」のリンクが正しくないのを修正。

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/rext-dev/rt-docs/tree/main/docs',
+            'https://github.com/rext-dev/rt-docs/tree/main',
         },
         blog: {
           showReadingTime: true,
